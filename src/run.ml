8f350c4373373652c4fe43a85a2053018381eef8
Katherine Whitlock <toroidalcode@gmail.com>
2014-02-27 11:59:14 -0500

    Starting builder

--- src/run.ml
+++ src/run.ml
@@ -17,14 +17,7 @@ let to_map in_json =
 let print_map map =
   StringMap.iter map ~f:(fun ~key ~data ->
       printf "%s => %s\n" key data)
-
-let command =
-  Command.basic
-    ~summary:"Run some arbitrary script file"
-    ~readme:(fun () -> "More detailed information")
-    Command.Spec.(empty +> anon ("filename" %: string))
-    (fun filename () -> print_string filename)
-
+    
 
 let command_map =   
   let home = match Sys.getenv "HOME" with
@@ -50,6 +43,33 @@ let command_map =
         | `Left user -> Some(user)
         | `Right sys -> Some(sys))
 
+
+
+
+let execute filename = 
+  let extension = match Filename.split_extension filename with
+    | _, Some(ext) -> ext
+    | _, None -> 
+      failwith "Run could not determine how to run this file because it has no extension."
+  in
+  let run_cmd = match StringMap.find command_map "extension" with
+  | Some(cmd) -> Str.global_replace (Str.regexp "%") filename cmd 
+  | None -> failwith "Run could not determine how to run this file because it has an unknown extension."
+  in
+  let result = Unix.system run_cmd in
+  match result with
+  | Ok(_) -> exit 0
+  | Error exn -> failwith (Unix.Exit_or_signal.to_string_hum result)
+
+
+
+let command =
+  Command.basic
+    ~summary:"Run some arbitrary script file"
+    ~readme:(fun () -> "More detailed information")
+    Command.Spec.(empty +> anon ("filename" %: string))
+    (fun filename () -> execute filename)
+
+
 let () =
-  print_map command_map;
   Command.run ~version:"1.0" ~build_info:"RWO" command
